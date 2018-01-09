28c603d8b0653dbd9e81b24b1bc4a387d1ba45f0
Martin Dyring-Andersen <martin@dyring-andersen.dk>
2018-01-09 14:58:00 +0100

    Run as daemon

--- gaia/freebsd.sh
+++ gaia/freebsd.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 # Bootstrap pkg
-env ASSUME_ALWAYS_YES=YES pkg bootstrap
+env ASSUME_ALWAYS_YES=YES pkg bootstrap || true
 
 # Update packages - we'll reboot later on in case a this includes a kernel update
 pkg upgrade -y
@@ -17,8 +17,35 @@ go get github.com/cosmos/gaia || true
 cd $GOPATH/src/github.com/cosmos/gaia
 git checkout -qf v0.5.0
 make get_vendor_deps
-make install || true
+make install
 
 # Get testnet configuration
 git clone https://github.com/tendermint/testnets /opt/testnets
-gaia node start --home=/opt/testnets/gaia-2/gaia
+
+# Create gaia-daemon user and set permissions
+pw addgroup gaia-daemon
+pw adduser gaia-daemon -g gaia-daemon -d /nonexistant -s /usr/sbin/nologin
+touch /var/log/gaia.log
+chown -R gaia-daemon:gaia-daemon /opt/testnets /var/log/gaia.log
+
+# Create gaia service
+cat > /etc/rc.d/gaia << "EOF"
+#!/bin/sh
+
+# REQUIRE: LOGIN FILESYSTEMS
+. /etc/rc.subr
+
+name="gaia"
+rcvar=${name}_enable
+pidfile="/var/run/${name}.pid"
+command="/usr/sbin/daemon"
+command_args="-f -u gaia-daemon -P ${pidfile} -o /var/log/gaia.log /opt/gopath/bin/gaia node start --home=/opt/testnets/gaia-2/gaia"
+
+load_rc_config $name
+run_rc_command "$1"
+EOF
+chmod +x /etc/rc.d/gaia
+echo gaia_enable=YES >> /etc/rc.conf
+
+# Finally reboot
+reboot
